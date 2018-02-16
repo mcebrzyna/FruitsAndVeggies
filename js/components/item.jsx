60e947cfd1e9101fa279fbb722f4787f72a16e40
Michal Cebrzyna <mcebrzyna@gmail.com>
2018-02-16 15:42:33 +0100

    adding product amount

--- js/components/item.jsx
+++ js/components/item.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class Item extends React.Component{
+    state= {
+        amount: 1,
+    };
+
+    handleMinus = () => {
+        let currAmount = this.state.amount;
+        if(currAmount > 1){
+            this.setState({ amount: --currAmount})
+        }
+    };
+
+    handlePlus = () => {
+        let currAmount = this.state.amount;
+        this.setState({ amount: ++currAmount})
+    };
 
     render(){
         return (
@@ -9,9 +24,9 @@ class Item extends React.Component{
                 <div className='item-name'>{this.props.name}</div>
                 <div className='item-price'>{this.props.price} PLN</div>
                 <div className='amount-area'>
-                    <button className='minus'/>
-                    <input value='1'/>
-                    <button className='plus'/>
+                    <button className='minus' onClick={this.handleMinus}/>
+                    <input value={this.state.amount}/>
+                    <button className='plus' onClick={this.handlePlus}/>
                 </div>
                 <button className='addToCart-btn'>ADD TO CART</button>
             </div>
--- js/components/search.jsx
+++ js/components/search.jsx
@@ -5,13 +5,11 @@ class Search extends React.Component{
     render(){
         return (
             <div className='search-area'>
-                <form>
-                    <input className='text-input'
-                           value={this.props.text}
-                           onChange={this.props.handleText}
-                           placeholder='Search for Vegetables and Fruits'/>
-                    <input className='submit-btn' type="submit" value=''/>
-                </form>
+                <input className='text-input'
+                       value={this.props.text}
+                       onChange={this.props.handleText}
+                       placeholder='Search for Vegetables and Fruits'/>
+                <div/>
             </div>
         )
     }
