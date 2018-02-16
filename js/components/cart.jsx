cc513f3468ede9dc89561b08a1ea8b11d75e3f8a
Michal Cebrzyna <mcebrzyna@gmail.com>
2018-02-16 10:34:06 +0100

    hidding menu

--- js/components/cart.jsx
+++ js/components/cart.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 class Cart extends React.Component{
+    state = {
+        menuDisplay: 'off',
+    };
+
+    handleClick = () =>{
+        if(this.state.menuDisplay.indexOf('on') === -1){
+            this.setState( { menuDisplay: 'on',} );
+        }
+        else {
+            this.setState( {menuDisplay: 'off',} );
+        }
+    };
 
     render(){
         return (
@@ -14,9 +26,10 @@ class Cart extends React.Component{
                         <span>7</span>
                         <span>357</span>
                     </div>
-                    <div className='cart-icon'/>
+                    <div className='cart-icon' onClick={this.handleClick}/>
                 </div>
-                <div className='cart-container'>
+                <div className='cart-container'
+                     style={this.state.menuDisplay === 'on'? {display: 'block'} : {display: 'none'}}>
                     <div className='cart-items'>Empty</div>
                     <input type="submit" value='PROCEED TO CHECKOUT'/>
                 </div>
