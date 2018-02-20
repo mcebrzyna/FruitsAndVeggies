32084be4636b876591b68c8385995b476062fded
Michal Cebrzyna <mcebrzyna@gmail.com>
2018-02-20 09:19:38 +0100

    zoom imgs

--- js/components/item.jsx
+++ js/components/item.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 class Item extends React.Component{
     state= {
         amount: 1,
+        zoom: false,
     };
+
+    componentDidMount() {
+
+    }
 
     handleMinus = () => {
         let currAmount = this.state.amount;
@@ -25,10 +30,32 @@ class Item extends React.Component{
         this.props.sendToCart(this.props.name, this.state.amount, this.props.price, this.props.src);
     };
 
+    zoomClick = () => {
+        this.setState( {zoom: !this.state.zoom} )
+    };
+
+    zoom = () => {
+        if(this.state.zoom){
+            return(
+                <div className='zoom'>
+                    <div>
+                        <img src={this.props.src} alt={this.props.name} onClick={this.zoomClick}/>
+                        <div>
+                            <span>{this.props.name}</span>
+                            <span>{this.props.price} PLN</span>
+                        </div>
+                        <button onClick={this.zoomClick}/>
+                    </div>
+                </div>
+            )
+        }
+        return null;
+    };
+
     render(){
         return (
             <div className='item'>
-                <img src={this.props.src}/>
+                <img src={this.props.src} onClick={this.zoomClick}/>
                 <div className='item-name'>{this.props.name}</div>
                 <div className='item-price'>{this.props.price} PLN</div>
                 <div className='amount-area'>
@@ -38,7 +65,9 @@ class Item extends React.Component{
                 </div>
                 <button className='addToCart-btn'
                         onClick={this.handleClick}
-                        data-name={this.props.name}>ADD TO CART</button>
+                        data-name={this.props.name}>ADD TO CART
+                </button>
+                {this.zoom()}
             </div>
         )
     }
